c7684d92952cefb7ea3c7abad427c5d78f4b4e66
Michel Valencia Hernández <Michel Valencia Hernndez>
2017-05-04 19:33:12 -0500

    commit creación de proyecto laravel con ajax y jquery

---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Panel Administrativo</div>
+
+                <div class="panel-body">
+                    <p>
+                        <span id="products-total">{{ $products->total() }} </span>
+                        registros | página {{$products->currentPage()}} de {{ $products->lastPage() }}
+                    </p>
+                    <div id="alert" class="alert alert-info">
+                        
+                    </div>
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th width="20px">ID</th>
+                                <th>Nombre del Producto</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($products as $item)
+                                <tr>
+                                    <td width="20px">
+                                        {{ $item->id }}
+                                    </td>
+                                    <td>
+                                        {{$item->name}}
+                                    </td>
+                                    <td width="20px">
+                                        {!! Form::open(['route'=> ['destroyProduct', $item->id], 'method'=>'DELETE']) !!}
+                                            <a href="#" class="btn-delete">Eliminar</a>
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                    {!!$products->render()!!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+    <script src="{{ asset('js/script.js') }}"></script>
+@endsection
